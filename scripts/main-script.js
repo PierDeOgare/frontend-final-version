ea15b66f4e52ac20adb9ffe1005ea9a02a29f5cc
PierDeOgare <157198389+PierDeOgare@users.noreply.github.com>
2026-02-25 07:44:48 +0300

    add modal logic

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -16,3 +16,49 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+//MODAL LOGIC
+document.addEventListener("DOMContentLoaded", function () {
+
+    const modal = new bootstrap.Modal(
+        document.getElementById('welcomeModal')
+    );
+
+    const storedName = localStorage.getItem("profileName");
+    const storedImage = localStorage.getItem("profileImage");
+
+    if (!storedName || !storedImage) {
+        modal.show();
+    } else {
+        document.getElementById("profileName").textContent = storedName;
+        document.getElementById("profileImage").src = storedImage;
+    }
+
+    const form = document.getElementById("welcomeForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById("userName").value;
+        const imageInput = document.getElementById("userImage");
+
+        if (imageInput.files.length === 0) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            const imageData = event.target.result;
+
+            document.getElementById("profileName").textContent = name;
+            document.getElementById("profileImage").src = imageData;
+
+            localStorage.setItem("profileName", name);
+            localStorage.setItem("profileImage", imageData);
+
+            modal.hide();
+        };
+
+        reader.readAsDataURL(imageInput.files[0]);
+    });
+
+});
